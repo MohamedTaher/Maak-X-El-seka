21ab00e2757b6192d1805dd15ab7851d4a41918a
MohamedTaher <mtaherhassan95@gmail.com>
2017-02-04 22:11:26 +0200

    sort requests

--- Backend/WebService/WebService/src/com/Model/Constants.java
+++ Backend/WebService/WebService/src/com/Model/Constants.java
@@ -1,12 +1,13 @@
 package com.Model;
 
 public class Constants {
-	//public static final String dpUrl = "jdbc:mysql://localhost:3306/taxiTest";
-	//public static final String dpUser = "root";
-	//public static final String dpPassword = "root";
+	public static final String dpUrl = "jdbc:mysql://localhost:3306/maakxelseka";
+	public static final String dpUser = "root";
+	public static final String dpPassword = "root";
 	
-	
+	/*
 	public static final String dpUrl = "jdbc:mysql://127.4.190.2:3306/maakxelseka";
 	public static final String dpUser = "adminTe8aVMx";
 	public static final String dpPassword = "Ev3srutdcuyQ";
+	*/
 }
